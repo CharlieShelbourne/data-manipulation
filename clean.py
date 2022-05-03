fe6d78b81343258d32e650752f3511da1bb32780
Charlie <charlie_shelbourne@hotmail.co.uk>
2022-05-03 15:03:27 +0100

    romved clean.py

--- clean.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-
-def clean(df):
-    df["valuation"] = df["valuation"].replace('[\$,]', '', regex=True).astype(float)
-    df["founded_year"] = df["founded_year"].replace('None', np.nan)
-    df["founded_year"] = pd.to_datetime(df["founded_year"].astype(str))
